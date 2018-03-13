61ba46f77dfc52d0212b263797165774a65bd9fb
shreya4496 <shreyagupta4496@gmail.com>
2018-03-13 10:42:29 +0530

    Structure Added

--- CrimeMapping/settings.py
+++ CrimeMapping/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crimeReporting',
+    'login',
+    'maps',
+    'prediction'
 ]
 
 MIDDLEWARE = [
