defa7580cd5209c3c2e715a5a85d7438e95805f4
apoorvasingh17 <apoorvasingh2811@gmail.com>
2018-03-14 19:22:09 +0530

    added fields in user and fir models

--- crimeReporting/models.py
+++ crimeReporting/models.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
 
+from django.contrib.auth.models import *
+
+import datetime
 # Create your models here.
 
 
@@ -12,13 +14,17 @@ class USER(models.Model):
   def __str__(self):
       return self.NAME
 
-class FIR_REPORT(models.Model):
+class FIR_REPORT(model.Model):
   CRIME_TYPE = models.CharField(max_length=100)
   LOCATION_LAT = models.IntegerField()
   LOCATION_LONG = models.IntegerField()
   CRIME_DES = models.CharField(max_length=1000)
+  PERSON_COMPLAINT = models.ForeignKey(max_length=100)
+  COMPLAINT_BY = models.CharField(max_length=100)
+  DATE_CRIME = models.DateField(_(u"Conversation Date"), blank=True)
+  TIME_CRIME = models.TimeField(_(u"Conversation Time"), blank=True)
+  FIR_LOC = models.CharField(max_length=100)
+  COMPLAINT_TIME = models.TimeField(_(u"Conversation Time"), blank=True)
 
   def __str__(self):
       return self.CRIME_TYPE
-  
-  
