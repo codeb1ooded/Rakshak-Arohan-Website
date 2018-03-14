c916d3998a0669fbfda7ed1d9a6de8ad2608edb3
apoorvasingh17 <apoorvasingh2811@gmail.com>
2018-03-14 18:10:15 +0530

    added models for user and fir

--- crimeReporting/models.py
+++ crimeReporting/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class USER(models.Model):
+  USER_REF = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="USER")
+  NAME = models.CharField(max_length=100)
+  PASSWORD= models.CharField(max_length=6)
+
+  def __str__(self):
+      return self.NAME
+
+class FIR_REPORT(model.Model):
+  CRIME_TYPE = models.CharField(max_length=100)
+  LOCATION_LAT = models.IntegerField()
+  LOCATION_LONG = models.IntegerField()
+  CRIME_DES = models.CharField(max_length=1000)
+
+  def __str__(self):
+      return self.CRIME_TYPE
+  
+  
