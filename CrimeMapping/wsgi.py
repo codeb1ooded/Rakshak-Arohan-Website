bdc4c431279cef94303d7f7e5f1e7e78f4054b26
Aishna Gupta <aishna987@yahoo.co.in>
2018-03-22 13:43:19 +0530

    'WhiteNoise'

--- CrimeMapping/wsgi.py
+++ CrimeMapping/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for CrimeMapping project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-
+import whitenoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CrimeMapping.settings")
